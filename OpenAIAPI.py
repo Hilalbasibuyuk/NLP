c77df0375fcb2acbb3fa24cf069b914b643ddb96
hilalbasibuyuk <hilal.basibuyuk21@ogr.atauni.edu.tr>
2024-08-04 12:59:25 +0300

    Try GPT 3.5 Turbo using OpenAI API

--- OpenAIAPI.py
+++ OpenAIAPI.py
@@ -1,7 +1,7 @@
 # %%
 import os
 from openai import OpenAI
-# %% Open AI'dan bir API key al ve onu sisteme kaydettiğin adı (for ex "OPENAI-KEY") yaz
+# %% Open AI'dan bir API key al ve onu sisteme kaydettiğin adı (for ex "OPENAI-KEY") yaz 
 client = OpenAI(api_key= os.environ.get("OPENAI-KEY"))
 
 completion = client.chat.completions.create(
@@ -13,6 +13,3 @@ completion = client.chat.completions.create(
 )
 
 print(completion.choices[0].message)
-
-
-# %%
