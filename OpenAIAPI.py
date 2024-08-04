a34ff7cf489caffe656d5fa0725a756430758d23
hilalbasibuyuk <hilal.basibuyuk21@ogr.atauni.edu.tr>
2024-08-04 10:43:19 +0300

    Try GPT 3.5 Turbo using OpenAI API

---
+++ OpenAIAPI.py
@@ -0,0 +1,18 @@
+# %%
+import os
+from openai import OpenAI
+# %% Open AI'dan bir API key al ve onu sisteme kaydettiğin adı (for ex "OPENAI-KEY") yaz
+client = OpenAI(api_key= os.environ.get("OPENAI-KEY"))
+
+completion = client.chat.completions.create(
+    messages=[{
+        "role":"user",
+        "content": "The coffee is extremely ..."
+    }],
+    model="gpt-3.5-turbo",
+)
+
+print(completion.choices[0].message)
+
+
+# %%
