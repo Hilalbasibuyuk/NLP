38a509d5fac29852a258d38a6adc1cf75dbc3271
hilalbasibuyuk <hilal.basibuyuk21@ogr.atauni.edu.tr>
2024-08-05 13:04:16 +0300

    Word Embeddings

---
+++ DataAugmentation/WordEmbeddings.py
@@ -0,0 +1,6 @@
+import nlpaug.augmenter.word as naw
+# !pip install gensim
+augmentation = naw.WordEmbsAug(model_type='glove', model_path='glove.6B.100d.txt', action="substitute") # Download the txt file
+
+text_original = 'The pen is mightier than the sword.'
+augmentation.augment(text_original)
